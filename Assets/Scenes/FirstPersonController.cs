51352a770598a42b162e9b64bda1b890d0397c77
Ken_Kaniff <66913506+kenkaniffer@users.noreply.github.com>
2024-12-27 19:21:25 +0100

    add springing

--- Assets/Scenes/FirstPersonController.cs
+++ Assets/Scenes/FirstPersonController.cs
@@ -4,9 +4,17 @@ using UnityEngine.Animations;
 public class FirstPersonController : MonoBehaviour
 {
     public bool CanMove { get; private set; } = true;  //does the player have control of the character <Not set outside>
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+
+    [Header("Functional Options")]
+    [SerializeField] private bool canSprint = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
     
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 3.0f; //defaut walk speed and gravity
+    [SerializeField] private float sprintSpeed = 6.0f;
     [SerializeField] private float gravity = 30.0f;
     
     [Header("Look Parameters")]
@@ -46,7 +54,7 @@ public class FirstPersonController : MonoBehaviour
 
     private void HandleMovementInput() //Keyboard Controller 
     {
-        currentInput = new Vector2 (walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+        currentInput = new Vector2 ((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
         //Reset Vecotr Direction for next movement 
         float moveDirectionY  = moveDirection.y;
         moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
